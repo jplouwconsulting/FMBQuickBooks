de788cdaf3293d1ea5416aafd1381d063c827395
Juan Louw <jplouw.consulting@gmail.com>
2018-08-28 16:37:39 +0100

    Create Create_spPatientDatatoTransfertoQB.sql

---
+++ Create_spPatientDatatoTransfertoQB.sql
@@ -0,0 +1,36 @@
+USE [FMBDataHub]
+GO
+
+/****** Object:  StoredProcedure [dbo].[spPatientDatatoTransfertoQB]    Script Date: 8/28/2018 10:36:47 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		JPLouw Consulting LTD
+-- Create date: 28 Aug 2018 @ 16:16
+-- Description:	Used to insert missing data from Patient data ready to be transfered to QuickBooks
+-- =============================================
+CREATE PROCEDURE [dbo].[spPatientDatatoTransfertoQB]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	MERGE [FMBDataHub].[dbo].[tblPatientAudit] AS TARGET
+	USING [DEMO].[dbo].[tblPatient] AS SOURCE
+		ON (TARGET.[PatientID] = SOURCE.[PatientID])
+	WHEN NOT MATCHED BY TARGET THEN 
+		INSERT (PatientID) VALUES (SOURCE.PatientID)
+	OUTPUT $action,
+		INSERTED.PatientID as SOURCEPatientID;
+END
+GO
+
+
